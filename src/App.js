95f72bd2a6ef62f4e4805d68deff76d0e7a97ec4
unknown <dev.mern.sujan@gmail.com>
2023-03-04 10:40:10 +0600

    react router setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Navbar from "./components/layout/Navbar.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout.jsx";
+
+let router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<RootLayout />}></Route>)
+);
 
 function App() {
-  return <Navbar />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const RootLayout = () => {
+  return <Navbar />;
+};
+
+export default RootLayout;
