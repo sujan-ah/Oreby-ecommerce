3573820ceee2d7c7429aa2c4e10b0a02e2f7530f
unknown <dev.mern.sujan@gmail.com>
2023-03-04 19:48:18 +0600

    Header-dropdown-feature-change

--- src/components/layout/Dropdown.jsx
+++ src/components/layout/Dropdown.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Dropdown = ({ children, className }) => {
-  return <div className={className}>{children}</div>;
+const Dropdown = ({ children, className, dropref }) => {
+  return (
+    <div className={className} ref={dropref}>
+      {children}
+    </div>
+  );
 };
 
 export default Dropdown;
