1d126629ba4e02ab6b1ddbcc84b50365872a9992
unknown <dev.mern.sujan@gmail.com>
2023-03-07 11:31:41 +0600

    user Dropdown design done

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,25 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 import Container from "./Container";
 import Dropdown from "./Dropdown";
 import Flex from "./Flex";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaUser } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { BsCartFill } from "react-icons/bs";
 import List from "./List";
 import ListItem from "./ListItem";
 import Search from "./Search";
 
 const Header = () => {
-  let [show, setShow] = useState(false);
-  let ref = useRef();
-
-  let handleDropdown = () => {
-    setShow(!show);
-  };
+  let [categoryDropDownShow, setCategoryDropDownShow] = useState(false);
+  let [userDropDownShow, setUserDropDownShow] = useState(false);
+  let categoryDropDownRef = useRef();
+  let userDropDownRef = useRef();
 
   useEffect(() => {
     document.body.addEventListener("click", (e) => {
-      if (ref.current.contains(e.target)) {
-        setShow(true);
+      if (categoryDropDownRef.current.contains(e.target)) {
+        setCategoryDropDownShow(true);
       } else {
-        setShow(false);
+        setCategoryDropDownShow(false);
+      }
+
+      if (userDropDownRef.current.contains(e.target)) {
+        setUserDropDownShow(true);
+      } else {
+        setUserDropDownShow(false);
       }
     });
   });
@@ -30,17 +36,14 @@ const Header = () => {
       <Container>
         <Flex className="flex justify-between">
           <div className="flex items-center">
-            <Dropdown dropref={ref}>
-              <p
-                onClick={handleDropdown}
-                className="flex items-center gap-x-2.5 "
-              >
+            <Dropdown className="relative" dropref={categoryDropDownRef}>
+              <p className="flex items-center gap-x-2.5 ">
                 <FaBars />
                 Shop by Category
               </p>
 
-              {show && (
-                <List className="font-regular absolute w-[263px] bg-primary font-dm text-sm text-white text-opacity-70">
+              {categoryDropDownShow && (
+                <List className="font-regular absolute top-10 left-0 w-[263px] bg-primary font-dm text-sm text-white text-opacity-70">
                   <ListItem
                     itemname="Accessories"
                     className="px-4 py-4 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
@@ -78,7 +81,30 @@ const Header = () => {
           </div>
 
           <div className="flex items-center">
-            <h1>ami</h1>
+            <Flex className="flex gap-x-10">
+              <Dropdown className="relative" dropref={userDropDownRef}>
+                <div className="flex">
+                  <FaUser />
+                  <IoMdArrowDropdown />
+                </div>
+
+                {userDropDownShow && (
+                  <List className="font-regular absolute top-9 right-0 w-[200px] border border-solid border-[#F0F0F0] bg-white font-dm text-sm font-normal text-primary text-opacity-70  ">
+                    <ListItem
+                      itemname="My Account"
+                      className="border-b border-solid border-[#F0F0F0]  px-4 py-4 duration-100 ease-in hover:bg-primary hover:font-bold hover:text-white "
+                    ></ListItem>
+                    <ListItem
+                      itemname="Log Out"
+                      className="px-4 py-4 duration-100 ease-in hover:bg-primary hover:font-bold hover:text-white"
+                    ></ListItem>
+                  </List>
+                )}
+              </Dropdown>
+              <div>
+                <BsCartFill />
+              </div>
+            </Flex>
           </div>
         </Flex>
       </Container>
