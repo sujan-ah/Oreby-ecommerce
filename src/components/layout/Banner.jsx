8ed17dcd402cf9ba7bd22d119f2c9a32b5affee6
unknown <dev.mern.sujan@gmail.com>
2023-03-15 12:59:20 +0600

    react slick slide completely done

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,40 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Images from "./Images";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
+  let [slickDotActive, setSlickDotActive] = useState("");
+
   var settings = {
     dots: true,
-    infinite: true,
+    beforeChange: (prev, next) => {
+      setSlickDotActive(next);
+    },
+    Infinity: true,
+    autoplay: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    appendDots: (dots) => (
+      <div
+        style={{
+          display: "inline-block",
+          position: "absolute",
+          top: "58%",
+          left: "8%",
+          transform: "translateY(-50%)",
+          width: "3%",
+        }}
+      >
+        <ul
+          style={{
+            margin: "0px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        >
+          {" "}
+          {dots}{" "}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === slickDotActive
+            ? {
+                fontSize: "10px",
+                fontWeight: 400,
+                width: "30px",
+                color: "#000",
+                borderRight: "3px #262626 solid",
+              }
+            : {
+                width: "30px",
+                color: "#000",
+                borderRight: "3px white solid",
+                color: "transparent",
+              }
+        }
+      >
+        0{i + 1}
+      </div>
+    ),
   };
+
   return (
-    <div>
-      <h2>Single Item</h2>
-      <Slider {...settings}>
-        <div className="h-96 bg-green-500">
-          <h3>1</h3>
+    <Slider {...settings}>
+      <Link to="#">
+        <div>
+          <Images imgsrc="assets/banner.png" />
         </div>
-        <div className="h-96 bg-green-500">
-          <h3>2</h3>
+      </Link>
+      <Link to="#">
+        <div>
+          <Images imgsrc="assets/banner.png" />
         </div>
-        <div className="h-96 bg-green-500">
-          <h3>3</h3>
+      </Link>
+      <Link to="#">
+        <div>
+          <Images imgsrc="assets/banner.png" />
         </div>
-        <div className="h-96 bg-green-500">
-          <h3>4</h3>
-        </div>
-        <div className="h-96 bg-green-500">
-          <h3>5</h3>
-        </div>
-        <div className="h-96 bg-green-500">
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
+      </Link>
+    </Slider>
   );
 };
 
