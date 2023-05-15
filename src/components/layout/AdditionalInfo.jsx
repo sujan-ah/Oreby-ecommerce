7f2fbedb4b595a21668770233f9866bff720aa49
unknown <dev.mern.sujan@gmail.com>
2023-05-15 11:18:11 +0600

    Banner bottom responsive

--- src/components/layout/AdditionalInfo.jsx
+++ src/components/layout/AdditionalInfo.jsx
@@ -10,21 +10,21 @@ const AdditionalInfo = ({ children, className }) => {
     <div className="mt-[-6px] border border-solid border-[#F0F0F0] py-7">
       <Container>
         <Flex className="flex justify-between">
-          <Flex className="flex gap-x-4 text-center">
-            <TbNumber2 className="text-2xl" />
-            <p className="font-regular font-dm text-base text-[#6d6d6d]">
+          <Flex className="flex gap-x-1 text-center sm:gap-x-4">
+            <TbNumber2 className="text-sm sm:text-2xl" />
+            <p className="font-regular font-dm text-[10px]	 text-[#6d6d6d] sm:text-base">
               Two years warranty
             </p>
           </Flex>
-          <Flex className="flex gap-x-4 text-center">
-            <FaCarSide className="text-2xl" />
-            <p className="font-regular font-dm text-base text-[#6d6d6d]">
+          <Flex className="flex gap-x-1 text-center sm:gap-x-4">
+            <FaCarSide className="text-sm sm:text-2xl" />
+            <p className="font-regular font-dm text-[10px]	 text-[#6d6d6d] sm:text-base">
               Free shipping
             </p>
           </Flex>
-          <Flex className="flex gap-x-4 text-center">
-            <RxReload className="text-2xl" />
-            <p className="font-regular font-dm text-base text-[#6d6d6d]">
+          <Flex className="flex gap-x-1 text-center sm:gap-x-4">
+            <RxReload className="text-sm sm:text-2xl" />
+            <p className="font-regular font-dm text-[10px]	 text-[#6d6d6d] sm:text-base">
               Return policy in 30 days
             </p>
           </Flex>
