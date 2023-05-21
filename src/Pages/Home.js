9753162e6cbd952766d569fe92c49ba2d0dd397b
unknown <dev.mern.sujan@gmail.com>
2023-05-21 08:46:02 +0600

    advertise part done with responsive

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AdditionalInfo from "../components/layout/AdditionalInfo";
 import Banner from "../components/layout/Banner";
+import Advertise from "../components/layout/Advertise";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <AdditionalInfo />
+      <Advertise />
     </>
   );
 };
